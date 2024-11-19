6f8de55913e35ccb7756f81732cbaa39c5fdf53a
“Antonio” <carloserrea179@gmail.com>
2024-11-18 23:58:11 -0500

    TODO V1.5

---
+++ JS/AgregarFiltroAyuda.js
@@ -0,0 +1,32 @@
+var htmlFiltros = /*html*/`
+<nav>
+    <ul class="lista">
+        <li class="item-open">
+            <div class="item-boton">
+                <a href="" class="nav-link">Centro de ayuda</a>
+            </div>
+        </li>
+        <li class="item-open">
+            <div class="item-boton">
+                <a href="" class="nav-link">Libro de Reclamaciones</a>
+            </div>
+        </li>
+        <li class="item-open">
+            <div class="item-boton">
+                <a href="" class="nav-link">Preguntas Frecuentes</a>
+            </div>
+        </li>
+        <li class="item-open">
+            <div class="item-boton">
+                <a href="" class="nav-link">Horarios de atención</a>
+            </div>
+        </li>
+        <li class="item-open">
+            <div class="item-boton">
+                <a href="" class="nav-link">Políticas de privacidad</a>
+            </div>
+        </li>
+    </ul>
+</nav>
+`
+document.getElementsByClassName('filtros')[0].innerHTML = htmlFiltros
