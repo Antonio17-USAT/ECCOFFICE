96840e807ea72f6bacc6d4c435e1baeee3b1eba8
“Antonio” <carloserrea179@gmail.com>
2024-11-22 22:39:36 -0500

    TODO V3.0

--- JS/AgregarBarraLateral.js
+++ JS/AgregarBarraLateral.js
@@ -1,14 +1,14 @@
 var htmlBarraLateral = /*html*/`
-<h2>Menú</h2>
-            <a href="pagina1.html" class="menu-btn">Página 1</a>
-            <a href="pagina2.html" class="menu-btn">Página 2</a>
-            <div class="menu-group">
-                <button class="menu-btn dropdown-btn">Página 3 ▼</button>
-                <div class="dropdown-content">
-                    <a href="subpagina1.html" class="submenu-btn">Subpágina 3.1</a>
-                    <a href="subpagina2.html" class="submenu-btn">Subpágina 3.2</a>
-                </div>
-            </div>
-            <a href="pagina4.html" class="menu-btn">Página 4</a>
+    <button class="S"><a href="pagina1.html">Perfil</a></button>
+    <div class="menu-group">
+        <button class="menu-btn dropdown-btn">Compras ▼</button>
+        <div class="dropdown-content">
+            <a href="subpagina1.html" class="submenu-btn">Detalle Pedido</a>
+        </div>
+    </div>
+    <button class="S"><a href="pagina2.html">Lista de Deseos</a></button>
+    <button class="S"><a href="pagina4.html">Devoluciones</a></button>
+    <button class="S"><a href="pagina4.html">Configuración</a></button>
+   
 `
 document.getElementsByClassName('sidebar')[0].innerHTML = htmlBarraLateral
