b2340e1780fa5593cb48434476b830975cd2bd95
Your Name <you@example.com>
2024-12-03 23:35:38 -0500

    Rama 3

--- JS/TiendaCompleta.js
+++ JS/TiendaCompleta.js
@@ -115,7 +115,7 @@ if (typeof sitio_js !== 'undefined') {
                         </div>
                         <div class="boton">
                          <button class="btnEliminar" data-nombre="${item.nombre}">Eliminar</button>
-                    </div>
+                        </div>
                     </div>
                 </div>
             `;
